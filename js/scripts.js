f90eaa4ac9ed1df7ea67bbc06f936ca1f5ae6f2c
Erich Richter <erich.richter@gmail.com>
2020-01-22 01:52:02 -0800

    add sorting

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,40 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
+    
     var item1Input = $("input#item1").val();
     var item2Input = $("input#item2").val();
     var item3Input = $("input#item3").val();
     var item4Input = $("input#item4").val();
     
-
-    $(".person1").text(item1Input);
-    $(".person2").text(item2Input);
-    $(".animal").text(item3Input);
-    $(".exclamation").text(item4Input);
+    var groceries = [item1Input, item2Input,item3Input,item4Input];
     
-    $("#results").show();
+    // toUpper = function(x){ 
+    //   return x.toUpperCase();
+    // };
+    // array2 = array2.map(toUpper);
+    
+    
+    
+    var sortedGroceries = groceries.sort();
+    
+    
+    $("#listItem1").text(sortedGroceries[0]);
+    $("#listItem2").text(sortedGroceries[1]);
+    $("#listItem3").text(sortedGroceries[2]);
+    $("#listItem4").text(sortedGroceries[3]);
+    
+    
+    
+    
 
-    event.preventDefault();
+
+
+
+    $("#results").show();
+    console.log(groceries);
+    
   });
+  
+
 });
